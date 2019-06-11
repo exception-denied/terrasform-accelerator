1b923daef539685a91ba8611f1bd53bf9125c901
Mayank <mayank@krishnas-MacBook-Pro.local>
2019-06-11 22:48:27 +0530

    fix ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_cluster" "main" {
   name = "accelerator-${var.env}-cluster"
 }
 
-data "template_file" "${var.env}_app" {
+data "template_file" "accelerator_app" {
   template = "${file("templates/ecs/cb_app.json.tpl")}"
 
   vars {
@@ -21,7 +21,7 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "${var.fargate_cpu}"
   memory                   = "${var.fargate_memory}"
-  container_definitions    = "${data.template_file.${var.env}_app.rendered}"
+  container_definitions    = "${data.template_file.accelerator_app.rendered}"
 }
 
 resource "aws_ecs_service" "main" {
