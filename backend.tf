2fc88d3943651b82e4b20ad04e94aa3fdb0479e1
Mayank <mayank@krishnas-MacBook-Pro.local>
2019-06-11 22:09:14 +0530

    type in backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
    bucket = "hhh4563"
    region = "us-east-2"
-   key = "test/ecs/${var.env}/terraform.tfstate"
+   #key = "test/ecs/${var.env}/terraform.tfstate"
   }
 }
