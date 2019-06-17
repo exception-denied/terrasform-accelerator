c76f50f1bb06df1b307e22fc012362835ba6b625
krishna2751992 <35762116+krishna2751992@users.noreply.github.com>
2019-06-17 22:55:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-#  region                  = "${var.aws_region}"
+  region                  = "${var.aws_region}"
+  version = "~> 1.60"
  # shared_credentials_file = "~/.aws/credentials"
  # profile                 = "default"
 }
